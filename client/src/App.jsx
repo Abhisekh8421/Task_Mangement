39d36c6753e3d58a81b1d82d875b4f51a65ce334
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-10-30 20:37:12 +0530

    made some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,13 @@ import ScrumBoardPage from "./pages/ScrumBoard";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/auth" replace />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/tasks" element={<CommonLayout />}>
         <Route path="lists" element={<TaskPage />} />
         <Route path="scrum-board" element={<ScrumBoardPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/auth" replace />} />
     </Routes>
   );
 }
